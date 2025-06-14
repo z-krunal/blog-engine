73cdc85eb7a09368e28cf50dec222b61d57ee4a6
z-krunal <krunalsabnis.x@gmail.com>
2025-06-14 07:34:36 +0200

    chore: fix export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-export * from './components/BlogCoverImage';
-export * from './components/BlogHeader';
-export * from './components/MDXContent';
+export { default as BlogCoverImage } from './components/BlogCoverImage';
+export { default as BlogHeader } from './components/BlogHeader';
+export { default as MDXContent } from './components/MDXContent';
 
-export * from './components/MDXRenderer';
-export * from './components/Prose';
-export * from './components/ResponsiveImage';
-export * from './components/SEO';
-export * from './components/ShareButtons';
+export { default as MDXRenderer }   from './components/MDXRenderer';
+export { default as Prose }  from './components/Prose';
+export { default as ResponsiveImage } from './components/ResponsiveImage';
+export { default as SEO } from './components/SEO';
+export { default as ShareButtons } from './components/ShareButtons';
 
-export * from './types/blog';
+export  * from './types/blog';
 export * from './mdx';
+
+
